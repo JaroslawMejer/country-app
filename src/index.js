f49256dd7e6157343c71414956bede8cc7df826c
Jaroslaw Mejer <jaroslaw.mejer@gmail.com>
2018-08-11 13:11:44 +0200

    Add bootstrap and new routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
+import routes from './routes';
 import DevTools from './DevTools'
 import { getCountries } from './actions/actions-countries.js'
 
 render(
     <Provider store={store}>
-    	<div>
-        	<h1>Inicjalizacja projektu</h1>
-        	<DevTools />
-        </div>
-    </Provider>,
+		<Router history={hashHistory} routes={routes} />
+	</Provider>,
     document.getElementById('root')
 );
 
